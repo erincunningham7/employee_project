ef533ea3f5b8f705ab050238e48a7f8b7d56586b
Erin Cunningham <cunninnghamerin@gmail.com>
2023-11-05 23:54:09 +0000

    change order of form fields

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -5,4 +5,4 @@ class EmployeeForm(forms.ModelForm):
 
     class Meta:
         model = Employee
-        fields = ('fullname', 'emp_code', 'mobile', 'position')
+        fields = ('fullname', 'mobile', 'emp_code', 'position')
